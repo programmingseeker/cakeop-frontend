26667dd98c9ce6a467447aeaf4ef446aa3463992
Krtin Shet <krtin71@gmail.com>
2020-09-20 23:48:20 -0700

    color refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,12 @@
-var modal = document.getElementById('myModal');
+const navbar = document.getElementsByClassName('navbar')[0];
+const bannerSection = document.getElementsByClassName('banner')[0];
 
-// Get the image and insert it inside the modal, using its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById('imgUser');
-var captionText = document.getElementById('caption');
-img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+window.addEventListener('scroll', () => {
+  console.log(window.pageYOffset);
+  navbar.style.transition = '300ms';
+  if (window.pageYOffset >= 150) {
+    navbar.style.background = '#ffffff';
+  } else {
+    navbar.style.background = 'transparent';
+  }
+});

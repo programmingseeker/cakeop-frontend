c93967779c181d8391a04b2812364e1972526df5
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2020-09-23 23:25:52 +0530

    Completed Account page with minor refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -15,34 +15,26 @@ window.addEventListener('scroll', () => {
 /////////////////////////////////////////////////////////////////
 
 // This is a modal to view the Profile photo
+var check = document.getElementsByClassName('profile-photo');
 var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal, using its "alt" text as a caption
+// To check if the src file is present
 var img = document.getElementById('myImg');
+var url = img.attributes.src.value;
+var http = new XMLHttpRequest();
+// Get the image and insert it inside the modal, using its "alt" text as a caption
 var modalImg = document.getElementById('imgUser');
 var captionText = document.getElementById('caption');
 img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-
-span.onclick = function () {
-  modal.style.display = 'none';
-};
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal, using its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById('imgUser');
-var captionText = document.getElementById('caption');
-img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  http.open('HEAD', url, false);
+  http.send();
+  if (http.status === 200) {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  } else {
+    modal.style.display = 'none';
+  }
 };
 
 // Get the <span> element that closes the modal
